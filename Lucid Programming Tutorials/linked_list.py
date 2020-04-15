8a46c9ac0a9ee5ebfca004edc5d56a07b13f8c54
geoffreynyaga <geoffreynyagak@gmail.com>
2020-04-15 19:17:29 +0300

    linked list length and length_recursive

--- Lucid Programming Tutorials/linked_list.py
+++ Lucid Programming Tutorials/linked_list.py
@@ -52,7 +52,7 @@ class LinkedList:
             last_node = last_node.next
 
         if target_found == False:
-            print(f"did not find the node {target}")
+            # print(f"did not find the node {target}")
             return
 
     def delete(self, target_node):
@@ -73,18 +73,57 @@ class LinkedList:
                 next_node = None
                 return
 
+    def length(self):
+        if self.head == None:
+            print(0)
+            return
+        current_node = self.head
+        count = 0
+        while current_node != None:
+            count += 1
+            current_node = current_node.next
+
+        return count
+
+    def length_recursive(self, node):
+        if node == None:
+            print(0)
+            return 0
+
+        return 1 + self.length_recursive(node.next)
+
+    def swap_nodes(self, node1, node2):
+        # "A" "B" "C" "D"
+        # node1Node = Node(node1)
+        node2Node = Node(node2)
+
+        current_node = self.head
+        while current_node.next != None:
+            if current_node.next.data == node1:
+                node2Node.next = current_node.next
+                current_node.next = node2Node
+
+            current_node = current_node.next
+
 
 llist = LinkedList()
 llist.append("a")
 llist.append("b")
 llist.append("c")
 llist.append("d")
+llist.append("z")
+
 llist.pre_append("e")
 llist.append_after_node("f", "e")
 llist.print_list()
 print("=========")
 
 llist.delete("f")
+# print("=========")
+
+llist.swap_nodes("c", "d")
 
 
 llist.print_list()
+# print(llist.length(), "<list length")
+# print(llist.length_recursive(llist.head), "<list length recursive")
